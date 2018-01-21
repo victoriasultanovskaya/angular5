38a43014ed9a396354822a9fd8eeff41fb2b6698
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-21 20:53:40 +0100

    4.toTitleCase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {AuthorsComponent} from './authors/authors.component';
 import {AuthorsService} from "./authors.service";
 import {SummaryPipe} from "./summary.pipe";
 import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { FavoriteComponent } from './favorite/favorite.component';
         CourseComponent,
         AuthorsComponent,
         SummaryPipe,
-        FavoriteComponent
+        FavoriteComponent,
+        TitleCasePipe
     ],
     imports: [
         BrowserModule,
