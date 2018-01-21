f72d7690ffc338bb89425ef5a35c6963b8f6bc25
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-21 20:12:02 +0100

    4.How would you apply "glyphicon-star" class to an element if "isFavorite" is true?
      [class.glyphicon-star]="isFavorite"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {CoursesService} from "./courses.service";
 import {AuthorsComponent} from './authors/authors.component';
 import {AuthorsService} from "./authors.service";
 import {SummaryPipe} from "./summary.pipe";
+import { FavoriteComponent } from './favorite/favorite.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import {SummaryPipe} from "./summary.pipe";
         CoursesComponent,
         CourseComponent,
         AuthorsComponent,
-        SummaryPipe
+        SummaryPipe,
+        FavoriteComponent
     ],
     imports: [
         BrowserModule,
