d30235e3ef99977fc4bd32e85090b80f1e27c9d1
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-22 20:30:00 +0100

    5.61. ngContent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {AuthorsService} from "./authors.service";
 import {SummaryPipe} from "./summary.pipe";
 import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
+import { PanelComponent } from './panel/panel.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { TitleCasePipe } from './title-case.pipe';
         AuthorsComponent,
         SummaryPipe,
         FavoriteComponent,
-        TitleCasePipe
+        TitleCasePipe,
+        PanelComponent
     ],
     imports: [
         BrowserModule,
