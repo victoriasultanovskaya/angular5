a450229c3eb514833b08886014f730ef2f04ac03
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-23 14:07:36 +0100

    6.72. ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,6 @@ import {isBoolean} from "util";
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    courses = [
-        {id: 1, name: 'course1'},
-        {id: 2, name: 'course2'},
-        {id: 3, name: 'course3'},
-        {id: 4, name: 'course4'}
-    ];
+    canSave = true;
 }
 
