23906aa59e8921b2b2a64d6f642db376ca6623f4
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-23 19:59:52 +0100

    7. Template-driven Forms 77. Types of Forms

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+/**
+ * Types of Forms
+ * 1. Template-driven
+ * 2. Reactive
+ * @see angular5\src\assets\FormTypes.png
+ */
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
