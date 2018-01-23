080643fadcf82bd4eca1dd54cc42d8aaf614938b
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-23 23:47:47 +0100

    7.88. Template-driven Forms. Working with Drop-down Lists

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -13,12 +13,16 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+    contactMethods = [
+        {id: 1, name: 'Email'},
+        {id: 2, name: 'Phone'}
+    ];
 
     log(x) {
         console.log(x);
     }
 
-    submit(form){
+    submit(form) {
         console.log(form);
         console.log(form.value); //Send it to server
     }
