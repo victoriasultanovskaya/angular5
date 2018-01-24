bfbf451fcd06ea902392b9bdbb703f6766d8c55a
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-24 13:18:19 +0100

    8. 92. Reactive Forms: Creating Controls Programmatically

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'signup-form',
-  templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.css']
+    selector: 'signup-form',
+    templateUrl: './signup-form.component.html',
+    styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
+    signUpForm = new FormGroup({
+        usernameField: new FormControl(),
+        passwordField: new FormControl()
+    });
 }
