de07c613e80da0e9fb60efb18fda9b0a6e0761e9
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-24 17:46:22 +0100

    8. 102. Reactive Forms: FormBuilder

--- src/app/course-topic/course-topic.component.ts
+++ src/app/course-topic/course-topic.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'course-topic',
@@ -7,9 +7,18 @@ import {FormArray, FormControl, FormGroup} from "@angular/forms";
     styleUrls: ['./course-topic.component.css']
 })
 export class CourseTopicComponent {
-    form = new FormGroup({
-        topics: new FormArray([])
-    });
+    form;
+
+    constructor(fb: FormBuilder) {
+        this.form = fb.group({
+            name: ['', Validators.required],
+            contact: fb.group({
+                email: [],
+                phone: []
+            }),
+            topics: fb.array([])
+        });
+    }
 
     addTopic(topic) {
         this.topics.push(new FormControl(topic.value));
@@ -21,7 +30,7 @@ export class CourseTopicComponent {
     }
 
     removeTopic(topic: FormControl) {
-        let index = this.topics.controls.lastIndexOf(topic);
+        const index = this.topics.controls.lastIndexOf(topic);
         this.topics.removeAt(index);
     }
 }
