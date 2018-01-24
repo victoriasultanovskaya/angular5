76ea4835decc2cd2b3b1db9a53005f91f434b866
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-24 23:11:24 +0100

    9.115. Consuming HTTP Services: Handling Expected Errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -21,50 +21,66 @@ export class PostsComponent implements OnInit {
         input.value = '';
 
         this.service.createPost(post)
-            .subscribe(response => {
-                console.log(response.json());
-                post['id'] = response.json().id;
-                this.posts.splice(0, 0, post);
-            }, error => {
-                alert('An unexpected error occurred');
-                console.log(error);
-            });
+            .subscribe(
+                response => {
+                    console.log(response.json());
+                    post['id'] = response.json().id;
+                    this.posts.splice(0, 0, post);
+                },
+                (error: Response) => {
+                    if (error.status === 400) {
+                        //this.form.setErrors(error.json());
+                    } else {
+                        alert('An unexpected error occurred');
+                        console.log(error);
+                    }
+                });
     }
 
     updatePost(post) {
         //Use patch to update only a few of the properties
         this.service.updatePost(post)
-            .subscribe((response) => {
-                console.log(response.json());
-            }, error => {
-                alert('An unexpected error occurred');
-                console.log(error);
-            });
+            .subscribe(
+                response => {
+                    console.log(response.json());
+                },
+                error => {
+                    alert('An unexpected error occurred');
+                    console.log(error);
+                });
 
     }
 
     deletePost(post) {
         this.service.deletePost(post)
-            .subscribe((response) => {
-                console.log(response.json());
-                let index = this.posts.lastIndexOf(post);
-                this.posts.splice(index, 1);
-            }, error => {
-                alert('An unexpected error occurred');
-                console.log(error);
-            });
+            .subscribe(
+                response => {
+                    console.log(response.json());
+                    let index = this.posts.lastIndexOf(post);
+                    this.posts.splice(index, 1);
+                },
+                (error: Response) => {
+                    if (error.status === 404) {
+                        alert('This post has already been deleted.');
+                    } else {
+                        alert('An unexpected error occurred');
+                        console.log(error);
+                    }
+                });
     }
 
     ngOnInit() {
         console.log('INIT');
         this.service.getPosts()
         //"subscribe" method means - when result is ready we'll be notified
-            .subscribe(response => {
-                console.log(response.json());
-                this.posts = response.json();
-            }, error => {
-                alert('An unexpected error occurred');
-                console.log(error);
-            });
+            .subscribe(
+                response => {
+                    console.log(response.json());
+                    this.posts = response.json();
+                },
+                error => {
+                    alert('An unexpected error occurred');
+                    console.log(error);
+                });
     }
 }
