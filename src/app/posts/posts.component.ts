46c893e0681b4641a1b38a0b93b10208a70da387
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-24 19:26:52 +0100

    9.107. Consuming HTTP Services: Creating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -6,14 +6,29 @@ import {Http} from "@angular/http";
     templateUrl: './posts.component.html',
     styleUrls: ['./posts.component.css']
 })
+
+// @see angular5\src\assets\HTTPRequests.png
 export class PostsComponent {
     posts: any;
+    private url = 'https://jsonplaceholder.typicode.com/posts';
 
-    constructor(http: Http) {
-        http.get('https://jsonplaceholder.typicode.com/posts')
+    constructor(private http: Http) {
+        http.get(this.url)
             .subscribe(response => {
                 console.log(response.json());
                 this.posts = response.json();
             }); //"subscribe" method means - when result is ready we'll be notified
+    }
+
+    createPost(input: HTMLInputElement) {
+        let post = {title: input.value};
+        input.value = '';
+
+        this.http.post(this.url, JSON.stringify(post))
+            .subscribe(response => {
+                console.log(response.json());
+                post['id'] = response.json().id;
+                this.posts.splice(0, 0, post);
+            });
     }
 }
