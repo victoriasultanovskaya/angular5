5b514bce77cc0a62ffca206b4fba85ad042bccab
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-26 14:47:49 +0100

    10.135. Routing and Navigation: Subscribing to Multiple Observables In order to Subscribe to Multiple Observables Instead of having two subscriptions to two Observables we nee to combine two observables to a new observable and then we'll subscribe to that combined observable

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {GithubFollowersService} from '../services/github-followers/github-followers.service';
 import {ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
     selector: 'github-followers',
@@ -14,21 +16,35 @@ export class GithubFollowersComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.route.paramMap
-            .subscribe(
-                params => {
+        /**
+         * In order to Subscribe to Multiple Observables
+         * Instead of having two subscriptions to two Observables
+         * we nee to combine two observables to a new observable
+         * and then we'll subscribe to that combined observable
+         * @see angular5\src\assets\Subscribing_to_Multiple_Observables.png
+         */
+        Observable.combineLatest([
+            this.route.paramMap,
+            this.route.queryParamMap
+        ])
+            .subscribe(combined => {
+                console.log(combined);
+                let userId = +combined[0].get('userId');
+                console.log(userId);
 
-                }
-            );
+                /**
+                 * combined[0] same as this.route.paramMap
+                 * combined[1] same as this.route.queryParamMap
+                 */
+                let page = +combined[1].get('page');
+                console.log(page);
 
-        this.route.queryParamMap.subscribe(
-            queryParam => {
+                // this.service.getAll({id: userId, page: page})
+                //     .subscribe(followers => this.followers = followers);
 
-            }
-        );
-
-        this.service.getAll()
-            .subscribe(followers => this.followers = followers);
+                this.service.getAll()
+                    .subscribe(followers => this.followers = followers);
+            });
 
 
     }
