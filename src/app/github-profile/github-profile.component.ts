14a14a14f9950cdb5bb00a15782c897bd2477767
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-01-26 13:51:57 +0100

    10.131. Routing and Navigation: Getting the Route Parameters

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-github-profile',
-  templateUrl: './github-profile.component.html',
-  styleUrls: ['./github-profile.component.css']
+    selector: 'app-github-profile',
+    templateUrl: './github-profile.component.html',
+    styleUrls: ['./github-profile.component.css']
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+    constructor(private route: ActivatedRoute) {
+        this.route.paramMap
+            .subscribe(params => {
+                console.log(params);
+                /*Plus before string converts string to number*/
+                let userId = +params.get('userId');
+                console.log(userId);
+            });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
