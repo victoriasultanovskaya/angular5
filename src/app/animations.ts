18497861165fc436476a7d450237e2abf71a0e4d
Viktoriia Sultanovska <victoria.sultanovska@gmail.com>
2018-02-17 20:22:39 +0100

    14.196. Animations: Easings http://cubic-bezier.com/

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -15,9 +15,9 @@ export let slideLeftToRight = trigger('slideLeftToRight', [
     state('void', style({opacity: 0})),
     transition(':enter', [
         style({transform: 'translateX(-10px)'}),
-        animate(1000)
+        animate('0.5s 0s cubic-bezier(.06,.99,.89,.06)')
     ]),
     transition(':leave', [
-        animate(500, style({transform: 'translateX(-100%)'}))
+        animate('0.5s 1s ease-in', style({transform: 'translateX(-100%)'}))
     ])
 ]);
