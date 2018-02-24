38bbc65ea39d8d5176efaed1a80093f50cf0f2ab
Viktoriia Sultanovska <victoria.sultanovska@gmail.com>
2018-02-25 00:41:24 +0100

    Auto-generated commit

--- ts/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * tsc main.ts --target ES5 && node main.js
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-var like_component_1 = require("./like.component");
-var like = new like_component_1.LikeComponent(10, true);
-like.onClick();
-console.log("likesCount: " + like.likesCount + ", isSelected: " + like.isSelected);
--- ts/main.ts
+++
@@ -1,9 +0,0 @@
-/**
- * tsc main.ts --target ES5 && node main.js
- */
-
-import {LikeComponent} from './like.component';
-
-let like = new LikeComponent(10, true);
-like.onClick();
-console.log(`likesCount: ${like.likesCount}, isSelected: ${like.isSelected}`);
